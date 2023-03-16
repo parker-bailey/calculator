82cb801cf05868b533b9801d7d2634879dddb95e
parker-bailey <103592959+parker-bailey@users.noreply.github.com>
2023-03-16 08:16:50 -0600

    Add files via upload
    This is the updated work to my project

--- app.js
+++ app.js
@@ -1,2 +1,26 @@
-let firstNumber = 0;
-const additionButton = document.querySelector('.plus');
+let firstNumber = '';
+let secondNumber = '';
+
+const input = document.querySelector('.calculator');
+const additionButton = document.querySelector('#plus');
+const subtractionButton = document.querySelector('#minus');
+const multiplicationButton = document.querySelector('#multiply');
+const divisionButton = document.querySelector('#divide');
+
+const allRows = document.querySelector('#allrows');
+
+const enter = document.querySelector('#enter');
+const clear = document.querySelector('#clear');
+
+allRows.addEventListener('click', e => {
+    if (e.target.classList.contains('number')){
+        firstNumber += e.target.id;
+        input.value = firstNumber;
+    }
+});
+
+clear.addEventListener('click', () => {
+    firstNumber = '';
+    secondNumber = '';
+    input.value = 0;
+})
